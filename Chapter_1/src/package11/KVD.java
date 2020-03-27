9d9b0ca03e1f3b32e6d0b2fbbc391e466ac794b9
tieugiang91 <tieugiang91@gmail.com>
2020-03-28 00:22:16 +0900

    GiangNCT-Chapter1.1

---
+++ Chapter_1/src/package11/KVD.java
@@ -0,0 +1,17 @@
+package package11;
+
+public class KVD {
+
+	public static void main(String[] args) {
+		System.out.println("**        ***    **********    **              *             **");
+		System.out.println("**      ***      **        **   **            ***           **");
+		System.out.println("**    ***        **         **   **          ** **         **");
+		System.out.println("**  ***          **          **   **        **   **       **");
+		System.out.println("*****            **          **    **      **     **     **");
+		System.out.println("**  ***          **         **      **    **       **   **");
+		System.out.println("**    ***        **        **        **  **         ** **");
+		System.out.println("**      ***      **       **          ***            ***");
+		System.out.println("**        ***    **********            *              *");
+	}
+
+}
