2a4bdaa1936872072c829b253b33a611cfd37598
VuAnhIt <vutheanhit@gmail.com>
2020-04-19 11:03:07 +0700

    Refactoring the creative exercises and exercises into more friendly methods which do not use the input from String array (args) in the main method. Added switch case in DayOfWeek.class which is my favorite algorithm of the day.

---
+++ Chapter_1/VuAnhIT/src/com/anhvt/chapter1_2/creative_exercises/DayOfWeek.java
@@ -0,0 +1,82 @@
+package com.anhvt.chapter1_2.creative_exercises;
+
+/******************************************************************************
+ *  Compilation:  javac DayOfWeek.java
+ *  Execution:    java DayOfWeek m d y
+ *
+ *  Read in the month (m), day (d), and year (y) and print out what
+ *  day of the week it falls on according to the Gregorian calendar.
+ *  For M use 1 for January, 2 for February, and so forth. Outputs
+ *  0 for Sunday, 1 for Monday, and so forth.
+ *
+ *        y0 = y - (14 - m) / 12
+ *        x = y0 + y0/4 - y0/100 + y0/400
+ *        m0 = m + 12 * ((14 - m) / 12) - 2
+ *        d = (d + x + (31*m0)/12) mod 7
+ *
+ *
+ *  % java DayOfWeek 8 2 1953      // August 2, 1953
+ *  0                              // Sunday
+ *
+ *  % java DayOfWeek 1 1 2000      // January 1, 2000
+ *  6                              // Saturday
+ *
+ ******************************************************************************/
+public class DayOfWeek {
+    public static void main(String[] args) {
+        dayOfWeek(8, 2, 1953);
+        dayOfWeek(1, 1, 2000);
+        dayOfWeek(4, 19, 2020);
+    }
+
+    private static void dayOfWeek(int m, int d, int y) {
+        int y0 = y - (14 - m) / 12;
+        int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+        int m0 = m + 12 * ((14 - m) / 12) - 2;
+        int d0 = (d + x + (31 * m0) / 12) % 7;
+
+        switch (d0) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Something in method dayOfWeek is wrong!");
+        }
+    }
+}
+/*
+* Original class
+* public class DayOfWeek {
+    public static void main(String[] args) {
+        int m = Integer.parseInt(args[0]);
+        int d = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
+
+        int y0 = y - (14 - m) / 12;
+        int x = y0 + y0/4 - y0/100 + y0/400;
+        int m0 = m + 12 * ((14 - m) / 12) - 2;
+        int d0 = (d + x + (31*m0)/12) % 7;
+
+        System.out.println(d0);
+    }
+
+}
+*/
