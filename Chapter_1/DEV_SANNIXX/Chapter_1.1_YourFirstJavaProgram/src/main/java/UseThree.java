1a60d0f088f9ae1c3f04adbc718f19bbf77988c5
Sannixx <init0b101@gmail.com>
2020-04-19 14:32:13 +0700

    UseThree

---
+++ Chapter_1/DEV_SANNIXX/Chapter_1.1_YourFirstJavaProgram/src/main/java/UseThree.java
@@ -0,0 +1,8 @@
+import org.jetbrains.annotations.NotNull;
+
+public class UseThree {
+
+    public static void main(@NotNull String[] args) {
+        System.out.printf("Hi %s, %s, and %s.", args[2], args[1], args[0]);
+    }
+}
