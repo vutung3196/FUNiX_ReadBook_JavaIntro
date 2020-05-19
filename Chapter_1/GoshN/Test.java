4804ab1d11d5286b0cd4623a449fed7e08c760e0
Gosh-N <ncsonnguyen@gmail.com>
2020-05-19 13:03:25 +0700

    noi dung sua doi

--- Chapter_1/GoshN/Test.java
+++ Chapter_1/GoshN/Test.java
@@ -1,43 +1,19 @@
+package Newpagkage;
+
 public class Test {
 
-    public static void main(String [] args) {
-   
-    	int i =0;
-    	int meet = 0;
-    while (i<=60) {
-  meet++;
-    	if (Math.random() <0.1) i++;
-    	else;
-    	
-    	
-    	
-    	
-    	
-    	/*
-    	for (int i = 0; i <=60; i ++) {
-	   while 
-	   
-	   
-   }
-   int stake  = Integer.parseInt(args[0]);    // gambler's stating bankroll
-	        int goal   = Integer.parseInt(args[1]);    // gambler's desired bankroll
-	        int trials = Integer.parseInt(args[2]);    // number of trials to perform
-	   int bets = 0;        // total number of bets made
-       int wins = 0;        // total number of games won
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int N = Integer.parseInt(args[0]);
+		for (int i = 0; i < N; i++)
+			   for (int j = 0; j < N; j++)
+			       if (i != j) System.out.print(i + ", " + j);
 
-       // repeat trials times
-       for (int t = 0; t < trials; t++) {
+			for (int i = 0; i < N; i++)
+			   for (int j = 0; (i != j) && (j < N); j++)
+			       System.out.println(i + ", " + j);
+			
 
-           // do one gambler's ruin simulation
-           int cash = stake;
-           while (cash > 0 && cash < goal) {
-               bets++;
-               if (Math.random() < 0.5) cash++;     // win $1
-               else                     cash--;     // lose $1
-           }
-           if (cash == goal) wins++;      */
-   }
-    System.out.println(meet);
-   
-    }
+	}
+
 }
