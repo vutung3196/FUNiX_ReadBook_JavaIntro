50aec65e724b1d0b4f789d0c996fd2b6530e2f9b
Gosh-N <ncsonnguyen@gmail.com>
2020-05-29 21:00:46 +0700

    BoyAndGirl.java

--- Chapter_1/GoshN/Chapter1/src/three/BoyAndGirl.java
+++ Chapter_1/GoshN/Chapter1/src/three/BoyAndGirl.java
@@ -4,52 +4,51 @@ public class BoyAndGirl {
 
 	public static void main(String[] args) {
 		int trials = Integer.parseInt(args[0]);
-		int i = 0 ;
-		int j = 0 ;
-		int k =1;
-		double c = Math.random();
-		double d = Math.random();
-		
+		int sum = 0;
+		int sb = 0;
+		int sg = 0;
+		int c2 = 0;
+		int c3 = 0;
+		int c4 = 0;
+		int cm = 0;		
 		for (int t = 0; t < trials; t++) {
-			if (Math.random() > 0.5)
-				i++;
+			boolean boy = false, girl = false;
+			int count = 0;
+			int i = 0 ;
+			int j = 0 ;
+			while (!(boy && girl)) {
+			double child = Math.random();
+			if (child < 0.5) {	
+			boy = true;  i++;
+			}
+			else {
+				girl = true; j++;
+			}
+			count++;
 			
+			}
 			
-
-			else 
-				j++;
-
+			if (count == 2) c2 +=1;
+			else if (count == 3) c3 +=1;
+			else if (count == 4) c4 +=1;
+			else cm +=1;
 			
-		 }
-		 System.out.println(i);
-		 System.out.println(j);
-
-		
-		/*int stake  = Integer.parseInt(args[0]);    // gambler's stating bankroll
-        int goal   = Integer.parseInt(args[1]);    // gambler's desired bankroll
-        int trials = Integer.parseInt(args[2]);    // number of trials to perform
-
-        int bets = 0;        // total number of bets made
-        int wins = 0;        // total number of games won
-
-        // repeat trials times
-        for (int t = 0; t < trials; t++) {
-
-            // do one gambler's ruin simulation
-            int cash = stake;
-            while (cash > 0 && cash < goal) {
-                bets++;
-                if (Math.random() < 0.5) cash++;     // win $1
-                else                     cash--;     // lose $1
-            }
-            if (cash == goal) wins++;                // did gambler go achieve desired goal?
-        }
-
-        // print results
-        System.out.println(wins + " wins of " + trials);
-        System.out.println("Percent of games won = " + 100.0 * wins / trials);
-        System.out.println("Avg # bets           = " + 1.0 * bets / trials);
-        */
+			sum += count;
+			sb +=i;
+			sg +=j;
+			
+		}
+		double k = (double)  sum /trials;
+		 System.out.println("Tong so tre em duoc sinh ra la = " + sum);
+		 System.out.println("Tong so be trai duoc sinh ra la = " + sb);
+		 System.out.println("Tong so be gai duoc sinh ra la = " + sg);
+		 System.out.println("Tong so gia dinh sinh 2 be la = " + c2);
+		 System.out.println("Tong so gia dinh sinh 3 be la = " + c3);
+		 System.out.println("Tong so gia dinh sinh 4 be la = " + c4);
+		 System.out.println("Tong so gia dinh sinh hon 4 be la = " + cm);
+		 System.out.println(" So tre em trung binh duoc sinh ra la = " + k);
 	}
-
-}
+}	 
+	
+		
+	
